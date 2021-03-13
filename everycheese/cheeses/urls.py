5d7f19a421dd5b70ebd2fba4d04ed59f1152fefe
Purnya Awasthi <purnya.awasthi.unh@gmail.com>
2021-03-13 16:41:28 -0500

    add cheese form added

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = "cheeses"
 urlpatterns = [
 path(route='',view=views.CheeseListView.as_view(),name='list'),
-# URL Pattern for the CheeseDetailView
+path(route='add/',view=views.CheeseCreateView.as_view(),name='add'),
 path(route='<slug:slug>/',view=views.CheeseDetailView.as_view(),name='detail'),
+
 ]
