5fd962b7bf6a599f6377c79fad137a38460bb72f
Purnya Awasthi <purnya.awasthi.unh@gmail.com>
2021-03-13 18:03:20 -0500

    add cheese, update cheese and modify view

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,6 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Cheese
-from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class CheeseListView(ListView):
@@ -12,7 +11,12 @@ class CheeseDetailView(DetailView):
 class CheeseCreateView(LoginRequiredMixin,CreateView):
     model = Cheese
     fields = ['name','description','firmness','country_of_origin',]
-    
+
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+
+class CheeseUpdateView(LoginRequiredMixin,UpdateView):
+    model = Cheese
+    fields = ['name','description','firmness','country_of_origin']
+    action = "Update"
